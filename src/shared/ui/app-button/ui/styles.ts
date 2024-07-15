d6015e6484a81fba754d9b838be6221b9d4cba3d
Victor Osipov <victorosipovprogrammer@gmail.com>
2024-07-15 13:05:28 +0300

    add class Styles

--- src/shared/ui/app-button/ui/styles.ts
+++ src/shared/ui/app-button/ui/styles.ts
@@ -1,33 +1,33 @@
-import { computed, type StyleHTMLAttributes, type StyleValue } from 'vue'
 import type { Props } from './types'
 import type { IStyles } from '@/shared/types'
+import { Styles } from '@/shared/lib'
 
-export function useStyles(props: Props) {
-    const styles: IStyles = {}
+export function useStyles(props: Props): IStyles {
+    const styles = new Styles()
 
     switch (props.color) {
         default:
-            styles.app_button = {
+            styles.add('app_button', {
                 'background-color': 'var(--dark-blue-color)',
                 color: 'white',
                 border: 'none'
-            }
+            })
             break
         case 'pale-yellow':
-            styles.app_button = {
+            styles.add('app_button', {
                 'background-color': 'var(--pale-yellow-color)',
                 color: 'var(--dark-blue-color)',
                 border: 'none'
-            }
+            })
             break
         case 'white':
-            styles.app_button = {
+            styles.add('app_button', {
                 'background-color': 'white',
                 color: 'var(--dark-blue-color)',
                 border: '1px solid var(--dark-blue-color)'
-            }
+            })
             break
     }
 
-    return styles
+    return styles.get()
 }
